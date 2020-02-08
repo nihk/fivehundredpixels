ff5bb64cdb84eedc97a43e58c1f268d6a5b622a6
Nick Rose <nicholas.rose@cbc.ca>
2020-02-08 16:20:19 -0500

    Remove unused import

--- app/src/main/java/nick/fivehundredpixels/initializers/TimberInitializer.kt
+++ app/src/main/java/nick/fivehundredpixels/initializers/TimberInitializer.kt
@@ -1,6 +1,5 @@
 package nick.fivehundredpixels.initializers
 
-import nick.fivehundredpixels.initializers.Initializer
 import nick.fivehundredpixels.BuildConfig
 import timber.log.Timber
 import javax.inject.Inject
