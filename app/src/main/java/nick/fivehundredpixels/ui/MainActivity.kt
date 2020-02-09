e050e8af6ac9c369e94bd323155c8fcca4ce68b4
Nick Rose <nicholas.rose@cbc.ca>
2020-02-09 18:58:20 -0500

    Refactor navigation logic to better support nested graphs, be less hacky, and better support navigation between sibling feature modules.

--- app/src/main/java/nick/fivehundredpixels/ui/MainActivity.kt
+++ app/src/main/java/nick/fivehundredpixels/ui/MainActivity.kt
@@ -1,10 +1,11 @@
 package nick.fivehundredpixels.ui
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavOptions
 import androidx.navigation.findNavController
 import nick.fivehundredpixels.R
+import nick.photodetails.ui.PhotoDetailsFragmentArgs
 import nick.photoshowcase.ui.OnPhotoClickedListener
-import nick.photoshowcase.ui.PhotoShowcaseFragmentDirections
 
 class MainActivity
     : AppCompatActivity(R.layout.activity_main)
@@ -12,6 +13,17 @@ class MainActivity
 
     override fun onPhotoClicked(id: Long) {
         findNavController(R.id.five_hundred_pixels_host)
-            .navigate(PhotoShowcaseFragmentDirections.toDetails(id))
+            .navigate(
+                R.id.photodetails_graph,
+                PhotoDetailsFragmentArgs.Builder(id)
+                    .build()
+                    .toBundle(),
+                NavOptions.Builder()
+                    .setEnterAnim(R.anim.nav_default_enter_anim)
+                    .setExitAnim(R.anim.nav_default_exit_anim)
+                    .setPopEnterAnim(R.anim.nav_default_pop_enter_anim)
+                    .setPopExitAnim(R.anim.nav_default_pop_exit_anim)
+                    .build()
+            )
     }
 }
